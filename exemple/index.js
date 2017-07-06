152a9c8ae716d788771605487ed402648d0e5c99
nathan <grimaudathan@gmail.com>
2017-07-06 11:43:13 +0200

    feat(exemple) added some use case

--- exemple/index.js
+++ exemple/index.js
@@ -7,4 +7,19 @@ document.addEventListener('DOMContentLoaded', ()=>{
         createIcon:true,
         defaultColor:'#212121'
     })
+
+    MdColorPicker({
+        elementName:"picker2",
+        createIcon:false,
+        defaultColor:'red'
+    })
+    const events = MdColorPicker({
+        elementName:"picker3",
+        createIcon:false,
+        defaultColor:'red'
+    })
+
+    events.on('color-changed',(color)=>{
+        document.getElementById("picker3").value = color
+    })
 });
